888a96eaaa602969f4971b5bf343f4b312346bf5
kharpe <kharpe@chicagobooth.edu>
2020-12-02 23:02:16 +0000

    user can add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,9 +25,9 @@ class CommentsController < ApplicationController
 
     if the_comment.valid?
       the_comment.save
-      redirect_to("/comments", { :notice => "Comment created successfully." })
+      redirect_to("/photos/#{the_comment.photo.id}", { :notice => "Comment created successfully." })
     else
-      redirect_to("/comments", { :alert => "Comment failed to create successfully." })
+      redirect_to("/photos/#{the_comment.photo.id}", { :alert => "Comment failed to create successfully." })
     end
   end
 
