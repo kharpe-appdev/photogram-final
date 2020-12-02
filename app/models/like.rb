372f0190bb8ba80b69e422df678cc27559acf757
kharpe <kharpe@chicagobooth.edu>
2020-12-02 14:20:30 +0000

    Direct / indirect associations and validations added to model files

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,14 @@
 #  photo_id   :integer
 #
 class Like < ApplicationRecord
+
+  # Direct associations
+  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "fan_id" })
+  belongs_to(:photo, { :required => false, :class_name => "Photo", :foreign_key => "photo_id", :counter_cache => true })
+
+  # Validations
+  validates(:photo_id, { :presence => true })
+  validates(:photo_id, { :uniqueness => { :scope => ["fan_id"], :message => "already liked" } })
+  validates(:fan_id, { :presence => true })
+
 end
